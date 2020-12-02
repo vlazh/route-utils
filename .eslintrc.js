aac1381e85dc91eb073630bce4c1e55aaee98b46
Vladimir <vzh.inbox@gmail.com>
2020-12-02 11:30:43 +0600

    Update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-  extends: require.resolve('@vzh/configs/eslint/ts.common.eslintrc.js'),
+  extends: require.resolve('@vzh/configs/eslint/common'),
   rules: {
     'import/no-unresolved': ['error', { ignore: ['history'] }],
   },
